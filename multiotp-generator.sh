6acdad87c0a3f8b9fa0720e02286e4de25df9876
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2018-02-08 18:19:06 +0600

    Added working script

---
+++ multiotp-generator.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Created by Yevgeniy Goncharov - https://sys-adm.in
+#
+# Script for generate Keys and system commands for MultiOTP Windows software
+#
+
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+# Retreive user name
+echo -n "Enter User name and press [ENTER]: "
+read user_name
+
+# Check user name
+if [[ -z "$user_name" ]]; then
+  echo "Please determine user name!"
+  exit 1
+else
+  # Generate hex key
+  SYSTEMKEY=$(for i in $(seq 1 20); do echo -n $(echo "obase=16; $(($RANDOM % 16))" | bc); done; echo)
+  # Convert to Base32
+  USERKEY=$(echo $SYSTEMKEY | xxd -r -p | base32)
+  # Generate Windows user password
+  USERPASS=$(date +%s | sha256sum | base64 | head -c 8 ; echo)
+
+  # Show info
+  echo -e "User name is: \e[92m$user_name\e[0m\nMultiOTP system key - \e[92m$SYSTEMKEY\e[0m \nUser GA key - \e[92m${USERKEY,,}\e[0m"
+  # Save info to file
+  echo -e "User name is: \e[92m$user_name\e[0m\nMultiOTP system key - \e[92m$SYSTEMKEY\e[0m \nUser GA key - \e[92m${USERKEY,,}\e[0m" >> $SCRIPT_PATH/log.txt
+
+  # Generate work command for implement user to Windows OS
+  echo -e "\nYou can create user in Windows: \e[92mnet user /add $user_name $USERPASS\e[0m"
+  echo -e "You can reset created user pass: \e[92mnet user $user_name new_password\e[0m"
+
+  # Generate MultiOTP commands
+  echo -e "\nMultiOTP command:\nmultiotp.exe -debug -create $user_name TOTP $SYSTEMKEY 6\n"
+  echo -e "\nMultiOTP additional commands:\nmultiotp.exe -display-log $user_name GOOGLE-AUTH-KEY"
+  echo -e "multiotp -qrcode $user_name > c:\MultiOTP\users\\$user_name.png"
+  exit 0
+fi
